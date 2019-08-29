3dea4f8c990fff6c704e9fc8d6426a18fa9d67a2
pablofunes <pabfun7@gmail.com>
2019-08-29 17:41:21 -0300

    Initial commit

---
+++ src/main/java/com/pfunes/loginspringapp/LoginspringappApplication.java
@@ -0,0 +1,13 @@
+package com.pfunes.loginspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginspringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginspringappApplication.class, args);
+	}
+
+}
